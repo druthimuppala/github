9cfc0197d8856ba28c0fff2a60078f28f5c125ec
Druthi Muppala <druthimuppala@gmail.com>
2024-07-26 10:53:46 -0500

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Druthi Muppala on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
